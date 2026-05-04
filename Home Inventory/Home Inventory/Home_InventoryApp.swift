673eab0b6ce096ffcbc26d1c97a629074686c5fe
Sean Dudzik <sean.dudzik@gmail.com>
2026-05-04 00:16:38 -0400

    Added initial iOS Build

---
+++ Home Inventory/Home Inventory/Home_InventoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Home_InventoryApp.swift
+//  Home Inventory
+//
+//  Created by Sean Dudzik on 5/3/26.
+//
+
+import SwiftUI
+
+@main
+struct Home_InventoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
